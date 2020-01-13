89b4f12db324f348cbb16feb7fd6fb788523f584
sasonhe <642139009@qq.com>
2020-01-13 18:23:26 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <van-nav-bar
+  title="标题"
+  left-text="返回"
+  right-text="按钮"
+  left-arrow
+  @click-left="onClickLeft"
+  @click-right="onClickRight"
+/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods:{
+    onClickLeft(){
+
+    },
+    onClickRight(){
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill'
+import 'amfe-flexible'
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import promise from 'es6-promise'
+promise.polyfill()
+import { NavBar } from 'vant'
+Vue.use(NavBar);
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
